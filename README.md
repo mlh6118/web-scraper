8605f50d79eb11253f66ed14fe88148470fb285a
Marni Hager <mlh6118@gmail.com>
2022-07-16 21:27:13 -0700

    Completed get_citations_needed_report and README.

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Lab - Class 17 
+## Web Scraping
+## Author: Marni Hager
+
+### Setup
+The following must be installed for the web scraping code to work:  
+* beautifulsoup4
+* requests
+
+### How to run the application:
+Enter a URL as an argument to the function being called that has "citation 
+needed" on the page. It is recommended to use a wikipedia page for this.
+
+### Tests
+Tests were run using print statements that have since been deleted.  
+
+The one notable test was using a paragraph that had two citations needed 
+ within it.  
+
+The one test that was not completed was ensuring that the return statements 
+for both functions worked the same as the print statements.  Attempts were 
+made to run the script from the command line and have the output got to a file 
+called "sampleoutput.txt", but none passed due to lack of knowing how to 
+properly call the script with an argument.
+
+##### Worked with: Aoife Mulligan, JJ Escandor
--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://en.wikipedia.org/wiki/Richard_I_of_England"
 
-page = requests.get(URL)
+def get_citations_needed_count(URL):
+    """
+    Arguments:
+        URL: web address to scrape
 
-soup = BeautifulSoup(page.content, "html.parser")
-
-
-def get_citations_needed_count():
+    Returns:
+        value: a count of all the citations needed on a web page.
+    """
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
     number_citations_needed = 0
     citations = soup.find_all("a")
     for citation in citations:
@@ -17,9 +20,19 @@ def get_citations_needed_count():
     return number_citations_needed
 
 
-def get_citations_needed_report():
-    pass
+def get_citations_needed_report(URL):
+    """
+    Arguments:
+        URL: web address to scrape.
 
-
-if __name__ == '__main__':
-    get_citations_needed_count()
+    Returns:
+        text: the text that requires a citation.
+    """
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    citations = soup.find_all("p")
+    for citation in citations:
+        if "citation needed" in citation.text:
+            text_of_citation = citation.text.split("citation needed")
+            citation_text = text_of_citation[0] + "citation needed]"
+            return citation_text
